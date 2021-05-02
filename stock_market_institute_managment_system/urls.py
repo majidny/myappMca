a3c273ee73159ee11244827cd672593b790f286c
MAJID <59989969+majidny@users.noreply.github.com>
2021-05-02 00:30:45 -0700

    fifth

--- stock_market_institute_managment_system/urls.py
+++ stock_market_institute_managment_system/urls.py
@@ -4,12 +4,13 @@ from  django.conf import settings
 from django.conf.urls.static import static
 app_name="myapp"
 urlpatterns=[
-    path('',views.logins),
+    path('login/',views.logins),
 
 
 
     path('admin_hom/',views.admin_home),
     path('abc/',views.abc),
+
 
 
     path('admin_add_batch_post/', views.admin_add_batch_post),
@@ -20,8 +21,15 @@ urlpatterns=[
 
     path('admin_add_career_post/', views.admin_add_career_post),
 
-    path('admin_edit_career/',views.admin_edit_career),
+
+    path('admin_edit_career/<str:id>',views.admin_edit_career),
     path('admin_edit_career_post/', views.admin_edit_career_post),
+    path('admin_view_career_update/',views.admin_view_career_update),
+    path('admin_view_career_search/',views.admin_view_career_search),
+    path('admin_view_career_del/<str:id>',views.admin_view_career_del),
+
+
+
 
 
     path('admin_add_course_post/',views.admin_add_course_post),
@@ -36,9 +44,12 @@ urlpatterns=[
    # path('admin_edit_faculty_post/', views.admin_edit_faculty_post),
     path('admin_view_faculty_edit/<str:id>',views.admin_view_faculty_edit),
     path('admin_faculty_update/',views.admin_faculty_update),
+    path('admin_view_faculty_search/', views.admin_view_faculty_search),
 
 
     path('admin_add_faculty_attendence/',views.admin_add_faculty_attendence),
+
+
 
     path('admin_add_salary/<str:id>',views.admin_add_salary),
     path('admin_add_salary_post/', views.admin_add_salary_post),
@@ -48,14 +59,21 @@ urlpatterns=[
 
 
     path('admin_add_student_post/', views.admin_add_student_post),
+    path('ajax_batch_by_crs/<str:cid>', views.ajax_batch_by_crs),
 
-    path('admin_edit_student/',views.admin_edit_student),
+    path('admin_edit_student/<int:id>',views.admin_edit_student),
     path('admin_edit_student_post/', views.admin_edit_student_post),
+
+    path('admin_view_student_search/',views.admin_view_student_search),
+    path('admin_view_student_del/<str:id>',views.admin_view_student_del),
 
 
 
     path('admin_view_archievment/',views.admin_view_archievment),
     path('admin_view_attendence/',views.admin_view_attendence),
+    path('admin_add_faculty_attendence_post/',views.admin_add_faculty_attendence_post),
+    path('admin_view_attendence_search/',views.admin_view_attendence_search),
+
 
     path('admin_view_batch/',views.admin_view_batch),
     path('admin_view_batch_search/',views.admin_view_batch_search),
@@ -80,8 +98,49 @@ urlpatterns=[
     path('admin_view_faculty_del/<str:id>', views.admin_view_faculty_del),
 
     path('admin_view_review/', views.admin_view_review),
+
     path('admin_view_salary/', views.admin_view_salary),
-    path('admin_view_student/', views.admin_view_student),
+    path('admin_view_salary_edit/<str:id>',views.admin_view_salary_edit),
+    path('admin_salary_update/',views.admin_salary_update),
+    path('admin_view_student/', views.admin_view_student,name="admin_view_student"),
+
+
+
+    path('ad_faculty_login/',views.ad_faculty_login),
+    path('ad_faculty_view_profile/',views.ad_faculty_view_profile),
+    path('ad_view_courses/',views.ad_view_courses),
+    path('ad_view_faculties/',views.ad_view_faculties),
+
+    path('ad_view_assigned_batches/',views.ad_view_assigned_batches),
+    path('ad_view_students/',views.ad_view_students),
+    path('and_study_materials/',views.and_study_materials),
+    path('and_add_study_material/',views.and_add_study_material),
+    path('fac_del_material/',views.faculty_del_material),
+    path('faculty_add_work/',views.faculty_add_work),
+    path('faculty_del_work/',views.faculty_del_work),
+    path('faculty_view_work/',views.faculty_view_work),
+    path('faculty_update_work/',views.faculty_update_work),
+    path('',views.newtem),
+
+    path('and_add_syllbus/',views.and_add_syllbus),
+    path('faculty_del_sylabus/',views.faculty_del_sylabus),
+    path('faculty_update_sylabus/',views.faculty_update_sylabus),
+    path('faculty_view_sylabus/',views.faculty_view_sylabus),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ]
@@ -89,4 +148,4 @@ urlpatterns=[
 
 # if settings.DEBUG:
 #
-#     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
